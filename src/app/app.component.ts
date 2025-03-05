a1d012f16725f915e7aea9360418c4297fc78b74
Mishr1233 <shivshankarmishra84351935@gmail.com>
2025-03-05 19:46:55 +0530

    [Shiv shankar] Add. Angular project by one way databinding return message Hello from BridgeLabz

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'HelloWorld';
+  imgUrl = "../assets/BridgeLabzLogo.avif";
+  url = "https://www.bridgelabz.com";
+  userName: string = "";
+
+  ngOnInit(): void{
+    this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event: any) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.imgUrl, "_blank");
+  }
+}
